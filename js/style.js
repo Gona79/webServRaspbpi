18f3b367438a484ad0d1618a3e8457c1f6269797
Gona79 <cg.vera.07@gmail.com>
2023-08-06 01:51:50 -0300

    Se actualiza responsive

--- js/style.js
+++ js/style.js
@@ -1,8 +1,17 @@
 var nav = document.getElementById("myNav");
 var toggle = document.querySelector(".nav-toogle");
+var mainContent = document.querySelector('.content'); // Aquí es donde hice la modificación
 
 toggle.addEventListener("click", function () {
     nav.classList.toggle("open");
+
+    // Si el menú está abierto, agrega el margen superior al contenido principal
+    // Si no está abierto, quita el margen
+    if (nav.classList.contains("open")) {
+        mainContent.style.marginTop = nav.offsetHeight + 'px'; // El margen es igual a la altura de 'nav'
+    } else {
+        mainContent.style.marginTop = '0';
+    }
 });
 
 var dropdownBtns = nav.getElementsByClassName("dropbtn");
